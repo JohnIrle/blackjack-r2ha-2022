7eed21a614daa03399acc4596451678839518f41
John Irle <irlejohn@gmail.com>
2023-02-21 13:43:09 -0600

    LAB #3A: Move classes to Hexagonal packages

--- src/main/java/com/r2ha/blackjack/adapter/in/console/ConsoleCard.java
+++ src/main/java/com/r2ha/blackjack/adapter/in/console/ConsoleCard.java
@@ -1,7 +1,7 @@
-package com.r2ha.blackjack;
+package com.r2ha.blackjack.adapter.in.console;
 
-import com.r2ha.blackjack.Card;
-import com.r2ha.blackjack.Rank;
+import com.r2ha.blackjack.domain.Rank;
+import com.r2ha.blackjack.domain.Card;
 import org.fusesource.jansi.Ansi;
 
 import static org.fusesource.jansi.Ansi.ansi;
